3c9edb00b483555efd9cfb1296763d51562e89c1
kiran68 <kiranurshil88@gmail.com>
2023-04-12 18:25:06 +0530

    Add: Check Validate Last Name

--- src/main/java/com/bridgelabz/userregistrationproblem/userRegistration.java
+++ src/main/java/com/bridgelabz/userregistrationproblem/userRegistration.java
@@ -6,23 +6,25 @@ import java.util.regex.Pattern;
 
 public class userRegistration {
 
-	public static void main(String[] args) {
-	    String regex = "^[A-Z][a-zA-Z]{2,}$";
-
-	    Scanner sc = new Scanner(System.in);
-	    System.out.println("Enter The First Name : ");
-		String data = sc.nextLine();
-        
+public static void main(String[] args) {
 		
+		System.out.println("Enter The Last Name : ");
+		Scanner sc = new Scanner(System.in);
+		String lastName = sc.nextLine();
+		
+	
+		String regex = ("^[A-Z][a-zA-Z]{2,}$");
 		Pattern pattern = Pattern.compile(regex);
 
-		Matcher matcher = pattern.matcher(data);
+		Matcher matcher = pattern.matcher( lastName);
 		boolean result = matcher.matches();
 
-	    if (result) {
-	        System.out.println("Valid first name");
-	    } else {
-	        System.out.println("Pattern not matched");
-	    }
+		if (result) {
+			System.out.println("Pattern Is Match Last Name Name");
+			System.out.println( "Last Name :"    + lastName);
+		} else {
+			System.out.println("Pattern not matches");
+		}
+
 	}
 }
