e0a93db3ed329dfede4867e49a796fa3fd735b06
kiran68 <kiranurshil88@gmail.com>
2023-04-14 11:50:32 +0530

    Refactor: Throw Exception CAse Of Invalid User Details

--- src/test/java/com/bridgelabz/userregistrationproblem/userRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistrationproblem/userRegistrationTest.java
@@ -10,29 +10,143 @@ public class userRegistrationTest {
 	 
 	 private String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 	    private Pattern pattern = Pattern.compile(regex);
-	
-	    @Test
-	    public void testValidEmails() {
-	        String[] validEmails = {
-	                "abc@yahoo.com",
-	                "abc-100@yahoo.com",
-	                "abc.100@yahoo.com",
-	                "abc111@abc.com",
-	                "abc-100@abc.net",
-	                "abc.100@abc.com.au",
-	                "abc@1.com",
-	                "abc@gmail.com.com",
-	                "abc+100@gmail.com"
-	        };
 
-	        for (String email : validEmails) {
-	            Matcher matcher = pattern.matcher(email);
-	            boolean result = matcher.matches();
-	            Assert.assertEquals(email + " should be a valid email", true, result);
+	    @Test
+	    public void testValidFirstName() {
+	        String firstName = "John";
+	        try {
+	            validateFirstName(firstName);
+	        } catch (InvalidFirstNameException e) {
+	           Assert.assertEquals("Valid first name should not throw exception", e.getMessage());
 	        }
 	    }
 
+	    @Test
+	    public void testInvalidFirstName() {
+	        String firstName = "";
+	        try {
+	            validateFirstName(firstName);
+	        } catch (InvalidFirstNameException e) {
+	            Assert.assertEquals("Invalid first name should throw exception", "Invalid first name", e.getMessage());
+	        }
+	    }
+
+	    @Test
+	    public void testValidLastName() {
+	        String lastName = "Doe";
+	        try {
+	            validateLastName(lastName);
+	        } catch (InvalidLastNameException e) {
+	            Assert.assertEquals("Valid last name should not throw exception", e.getMessage());
+	        }
+	    }
+
+	    @Test
+	    public void testInvalidLastName() {
+	        String lastName = "";
+	        try {
+	            validateLastName(lastName);
+	        } catch (InvalidLastNameException e) {
+	            Assert.assertEquals("Invalid last name should throw exception", "Invalid last name", e.getMessage());
+	        }
+	    }
+
+	    @Test
+	    public void testValidEmail() {
+	        String email = "john.doe@example.com";
+	        try {
+	            validateEmail(email);
+	        } catch (InvalidEmailException e) {
+	           Assert.assertEquals("Valid email should not throw exception", e.getMessage());
+	        }
+	    }
+
+	    @Test
+	    public void testInvalidEmail() {
+	        String email = "johndoe@example";
+	        try {
+	           validateEmail(email);
+	        } catch (InvalidEmailException e) {
+	            Assert.assertEquals("Invalid email should throw exception", "Invalid email", e.getMessage());
+	        }
+	    }
+
+	    @Test
+	    public void testValidMobile() {
+	        String mobile = "1234567890";
+	        try {
+	            validateMobile(mobile);
+	        } catch (InvalidMobileException e) {
+	            Assert.assertEquals("Valid mobile should not throw exception", e.getMessage());
+	        }
+	    }
+
+	    @Test
+	    public void testInvalidMobile() {
+	        String mobile = "123";
+	        try {
+	            validateMobile(mobile);
+	        } catch (InvalidMobileException e) {
+	            Assert.assertEquals("Invalid mobile should throw exception", "Invalid mobile", e.getMessage());
+	        }
+	    }
+
+	    @Test
+	    public void testValidPassword() {
+	        String password = "Abcd1234";
+	        try {
+	            validatePassword(password);
+	        } catch (InvalidPasswordException e) {
+	            Assert.assertEquals("Valid password should not throw exception", e.getMessage());
+	        }
+	    }
+
+	    @Test
+	    public void testInvalidPassword() {
+	        String password = "abcd";
+	        try {
+	            validatePassword(password);
+	        } catch (InvalidPasswordException e) {
+	            Assert.assertEquals("Invalid password should throw exception", "Invalid password", e.getMessage());
+	        }
+	    }
+
+	    private void validateFirstName(String firstName) throws InvalidFirstNameException {
+	        if (firstName.isEmpty()) {
+	            throw new InvalidFirstNameException("Invalid first name");
+	        }
+	    }
+
+	    private void validateLastName(String lastName) throws InvalidLastNameException {
+	        if (lastName.isEmpty()) {
+	            throw new InvalidLastNameException("Invalid last name");
+	        }
+	    }
+
+	    private void validateEmail(String email) throws InvalidEmailException {
+	        Matcher matcher = pattern.matcher(email);
+	        if (!matcher.matches()) {
+	            throw new InvalidEmailException("Invalid email");
+	        }
+	    }
+
+	    private void validateMobile(String mobile) throws InvalidMobileException {
+	        if (mobile.length() != 10) {
+	            throw new InvalidMobileException("Invalid mobile");
+	        }
+	    }
+
+	    private void validatePassword(String password) throws InvalidPasswordException {
+	        if (password.length() < 8) {
+	            throw new InvalidPasswordException("Invalid password");
+	        }
+	    }
+
+}
+
+
+
 	 
 	   
-	}
+	
 	    
